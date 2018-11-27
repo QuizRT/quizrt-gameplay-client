a8e98abd98baf5a711a0f4f88f426458d87d1a3b
VaishnaviSahu <vaishnavi.sahu@cgi.com>
2018-11-27 15:19:25 +0530

    ui homepage

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
 // import { HttpClient } from '@angular/common/http';
 import {MatDialog, MatDialogConfig} from '@angular/material';
 import {PlayersComponent} from '../players/players.component';
@@ -10,6 +10,7 @@ export interface DialogData {
   country:string;
   password:string;
 }
+
 
 @Component({
   selector: 'app-home-page',
