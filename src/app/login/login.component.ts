f8ad40ccef1dce6dd5ddfc2fed6f71538ed4f158
Adarshi Sinha <adarshisinha@gmail.com>
2018-12-05 22:53:58 +0530

    lets see

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -24,7 +24,7 @@ export class LoginComponent {
       this.service.GetUser(this.email, this.password).subscribe((result:any)=>
       {
         //this.token = JSON.parse(result);
-        window.location.href = "http://172.23.238.164:7000/quizmaster/";
+        window.location.href = "http://172.23.238.164:7000/gameplay/play";
         // this.token = result;
         console.log(result);
         this.AfterLogin();
