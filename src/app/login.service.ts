fd41ef58ddc409e9569756104a15ff3d9dcacee6
Nishant Jain <nishantkumarajain@gmail.com>
2018-12-09 23:22:58 +0530

    Updated login service to return if user is authenticated

--- src/app/login.service.ts
+++ src/app/login.service.ts
@@ -1,13 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { CookieService } from 'ngx-cookie-service';
+import * as jwtDecode from 'jwt-decode';
 
 @Injectable()
 export class LoginService {
-  constructor() {
-
+  private userLoggedIn: boolean;
+  constructor(private cookieService) {
+    const token = this.cookieService.get('UserLoginAPIToken');
+    if (token) {
+      this.userLoggedIn = true;
+    } else {
+      this.userLoggedIn = false;
+    }
   }
 
   isLoggedIn(): Observable<boolean> {
-    return of(true);
+    return of(this.userLoggedIn);
   }
 }
